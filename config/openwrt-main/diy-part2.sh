9fa0c05d53bc165fe3cabdcf62dc9aaa6de280e5
palink <yuhanghai@126.com>
2024-08-22 12:57:11 +0800

    Update diy-part2.sh

--- config/openwrt-main/diy-part2.sh
+++ config/openwrt-main/diy-part2.sh
@@ -15,9 +15,9 @@ sed -i 's/root:::0:99999:7:::/root:$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.::0:99999:7
 sed -i "s|DISTRIB_REVISION='.*'|DISTRIB_REVISION='R$(date +%Y.%m.%d)'|g" package/base-files/files/etc/openwrt_release
 echo "DISTRIB_SOURCECODE='official'" >>package/base-files/files/etc/openwrt_release
 
-git clone https://github.com/jerrykuku/luci-theme-argon.git package/luci-theme-argon
+git clone https://github.com/ppofp/luci-theme-argon.git package/luci-theme-argon
 
-git clone https://github.com/destan19/OpenAppFilter.git package/OpenAppFilter 
+git clone https://github.com/ppofp/OpenAppFilter.git package/OpenAppFilter 
 
 # Modify default IP（FROM 192.168.1.1 CHANGE TO 192.168.31.4）
 # sed -i 's/192.168.1.1/192.168.31.4/g' package/base-files/files/bin/config_generate
