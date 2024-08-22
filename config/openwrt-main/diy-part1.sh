ce1844f45d2121dfafd2733d92dcff6a7e6fa794
palink <yuhanghai@126.com>
2024-08-22 09:47:54 +0800

    Update diy-part1.sh

--- config/openwrt-main/diy-part1.sh
+++ config/openwrt-main/diy-part1.sh
@@ -15,6 +15,5 @@
 echo >> feeds.conf.default
 echo 'src-git istore https://github.com/linkease/istore;main' >> feeds.conf.default
 echo >> feeds.conf.default
-echo 'src-git nas https://github.com/linkease/nas-packages.git;master' >> feeds.conf.default
-echo 'src-git nas_luci https://github.com/linkease/nas-packages-luci.git;main' >> feeds.conf.default
+echo 'src-git-full fros https://github.com/openfros/packages.git' >> feeds.conf.default
 
